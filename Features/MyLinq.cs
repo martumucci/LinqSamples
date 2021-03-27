c71a039f523b3848178e1b7b639fd212c5e745bb
Martina <martina22.12@hotmail.com>
2021-03-27 17:22:33 -0300

    Named method

---
+++ Features/MyLinq.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Features.Linq
+{
+    public static class MyLinq
+    {
+        /// <summary>
+        /// returns de amount of items in an IEnumerable of T
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sequence"></param>
+        /// <returns></returns>
+        public static int Count<T>(this IEnumerable<T> sequence) //Extension method
+        {
+            int count = 0;
+            foreach (var item in sequence)
+            {
+                count += 1;
+            }
+            return count;
+        }
+    }
+}
---
+++ Features/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+//using Features.Linq; // My extension methods
+using System.Linq; // Linq extension methods
+
+namespace Features
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Employee> developers = new Employee[]
+            {
+                new Employee {Id = 1, Name = "Scott"},
+                new Employee {Id = 2, Name = "Chris"}
+            };
+
+            IEnumerable<Employee> sales = new List<Employee>()
+            {
+                new Employee {Id = 3, Name  = "Alex"}
+            };
+
+            //Console.WriteLine(sales.Count());
+            //Console.WriteLine(developers.Count());
+
+            //IEnumerator<Employee> enumerator = developers.GetEnumerator();
+            //while (enumerator.MoveNext())
+            //{
+            //    Console.WriteLine(enumerator.Current.Name);
+            //}
+
+            foreach (var employee in developers.Where(NameStartsWithS))
+            {
+                Console.WriteLine(employee.Name);
+            }
+        }
+
+        private static bool NameStartsWithS(Employee employee)
+        {
+            return employee.Name.StartsWith("S");
+        }
+    }
+}
