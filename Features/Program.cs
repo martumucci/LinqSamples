c2f26e0e29145ad9bb0844af0bf4cc9432217c10
Martina <martina22.12@hotmail.com>
2021-03-28 16:43:27 -0300

    Func and Action delegates with Lambda expressions

--- Features/Program.cs
+++ Features/Program.cs
@@ -9,6 +9,20 @@ namespace Features
     {
         static void Main(string[] args)
         {
+
+            Func<int, int> square = x => x * x;
+            Func<int, int, int> add = (x, y) =>
+            {
+                int temp = x + y;
+                return temp;
+            };
+
+            Action<int> write = x => Console.WriteLine(x); // does not have a return type (returns void)
+
+            write(square(add(3, 5)));
+
+            Console.WriteLine(square(add(3,5)));
+
             IEnumerable<Employee> developers = new Employee[]
             {
                 new Employee {Id = 1, Name = "Scott"},
