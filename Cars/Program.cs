c61db0d64ff03dafd39f2da43312a8708cf0ea77
Martina <martina22.12@hotmail.com>
2021-04-02 19:02:29 -0300

    working with LinQ operators

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -11,8 +11,15 @@ namespace Cars
         {
             var cars = ProcessFile("fuel.csv");
 
-            var query = cars.OrderByDescending(c => c.Combined)
+            var query = cars.Where(c => c.Manufacturer == "BMW" && c.Year == 2016)
+                            .OrderByDescending(c => c.Combined)
                             .ThenBy(c => c.Name); // using another orderBy would reorder everything
+
+            var top = cars.OrderByDescending(c => c.Combined)
+                          .ThenBy(c => c.Name)
+                          .First(c => c.Manufacturer == "BMW" && c.Year == 2016); //  .Last()
+
+            var result = cars.Any(c => c.Manufacturer == "Ford"); //  .All()
 
             foreach (var car in query.Take(10))
             {
@@ -22,12 +29,42 @@ namespace Cars
 
         private static List<Car> ProcessFile(string path)
         {
-            return File.ReadAllLines(path)
+            var query = File.ReadAllLines(path)
                 .Skip(1)
                 .Where(l => l.Length > 1)
                 .Select(Car.ParseFromCsv)
                 .ToList();
+
+            var query2 = File.ReadAllLines(path)
+                .Skip(1)
+                .Where(l => l.Length > 1)
+                .ToCar();
+
+            return query2.ToList();
         }
 
+    }
+
+    public static class CarExtensions
+    {
+        public static IEnumerable<Car> ToCar(this IEnumerable<string> source)
+        {
+            foreach (var line in source)
+            {
+                var columns = line.Split(',');
+
+                yield return new Car
+                {
+                    Year = int.Parse(columns[0]),
+                    Manufacturer = columns[1],
+                    Name = columns[2],
+                    Displacement = double.Parse(columns[3]),
+                    Cylinders = int.Parse(columns[4]),
+                    City = int.Parse(columns[5]),
+                    Highway = int.Parse(columns[6]),
+                    Combined = int.Parse(columns[7])
+                };
+            }
+        }
     }
 }
